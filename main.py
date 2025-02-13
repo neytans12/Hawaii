2b3c322faa8b74fb0221b841086bd3e98d51e5cd
neytans12 <159956446+neytans12@users.noreply.github.com>
2025-02-13 06:26:15 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,9 @@
 from requests import get
 import os
 import pandas as pd
+import random
 from datetime import date
+import menu
 
 
 if not os.path.exists("dados"):
@@ -11,7 +13,7 @@ if not os.path.exists("dados"):
 freeDictionaryAPI = "https://api.dictionaryapi.dev/api/v2/entries/en/" # Link usado para emtrar em contato com API
 
 # Requisitar e mostrar definição de palavra:
-def req_mostrar_palavra(palavra):
+def req_mostrar_palavra(palavra, caminho_notebook):
     # Requisitando dados da API
     # A função get retorna um objeto (não vimos isso em aula então nao entrarei em detalhes)
     # Ao chamar o metodo text, obtemos um string com todo o texto acessado atraves da URL
@@ -35,7 +37,7 @@ def req_mostrar_palavra(palavra):
             # Usei o match/case para tornar o codigo mais enxuto 
             match salvar_palavra:
                 case "S":
-                    salvar_palavra_notebook(palavra.capitalize())
+                    salvar_palavra_notebook(palavra.capitalize(), caminho_notebook)
                     break
                 case "N":
                     break
@@ -46,26 +48,24 @@ def req_mostrar_palavra(palavra):
         enter = input("Pressione enter para sair do dicionario ")
         os.system('cls' if os.name == 'nt' else 'clear')
 # Salvar palavra em notebook
-def salvar_palavra_notebook(palavra):
-    caminho = os.path.join("dados", "notebook.csv") # Criando um caminho valido tanto para windows quanto para linux
-    if os.path.exists(caminho):
-        # Se o caminho já existe, significa que o dicionario ja foi criado
+def salvar_palavra_notebook(palavra, caminho_notebook):
+    if os.path.exists(caminho_notebook):
+        # Se o caminho_notebook já existe, significa que o dicionario ja foi criado
         # Nesse caso, podemos apenas ler o arquivo CSV e fazer as devidas operações
-        df_notebook = pd.read_csv(caminho) # Lendo o arquivo
+        df_notebook = pd.read_csv(caminho_notebook) # Lendo o arquivo
         nova_palavra= pd.DataFrame({"Palavras": [palavra]}) # Criando daatframo com nova palavra
 
         df_notebook = pd.concat([df_notebook, nova_palavra], ignore_index=True, join="inner")# Unindo os dois dataframes
-        df_notebook.to_csv(caminho) # Escrevendo arquivo
+        df_notebook.to_csv(caminho_notebook) # Escrevendo arquivo
     else:
         # No segundo caso, teremos que criar o arquivo
         df_notebook = pd.DataFrame({"Palavras": [palavra]})
-        df_notebook.to_csv(caminho)
+        df_notebook.to_csv(caminho_notebook)
         
-def acessar_notebook():
-    caminho_csv = os.path.join('dados', 'notebook.csv')
+def acessar_notebook(caminho_notebook):
 
-    if os.path.exists(caminho_csv):
-        df_notebook = pd.read_csv(caminho_csv)
+    if os.path.exists(caminho_notebook):
+        df_notebook = pd.read_csv(caminho_notebook)
         valores = list(df_notebook["Palavras"]) # lista com as palavras
         if len(valores) == 0:
             print("Seu notebook está vazio.")
@@ -77,53 +77,82 @@ def acessar_notebook():
     else:
         print("Seu notebook está vazio.")
 
-def delete_notebook():
-    caminho = os.path.join("dados", "notebook.csv")
-    if os.path.exists(caminho):
-        os.remove(caminho)
+def delete_notebook(caminho_notebook):
+    if os.path.exists(caminho_notebook):
+        os.remove(caminho_notebook)
         print("Feito")
     else:
         print("Seu notebook não existe existe...")
     
 
-def acessar_quiz_diario(data):
-    caminhho_logs_quiz = os.path.join("dados",  )
+def gerar_quiz_diario(caminho_perguntas): # GEra as funções que serão usadas no Quiz
+    # Carregar o CSV
+    df = pd.read_csv(caminho_perguntas)
+    df = df.drop(columns=['Unnamed: 0'])
+
+    # Selecionar 3 perguntas aleatórias
+    perguntas_selecionadas = df.sample(n=3, random_state=None)
+
+    # Converter as perguntas selecionadas em uma lista de dicionários
+    quiz = perguntas_selecionadas.to_dict(orient='records')
+
+    # Remover as perguntas selecionadas do DataFrame original
+    df = df.drop(perguntas_selecionadas.index)
+    df = df.reset_index(drop=True)
+    # Salvar o DataFrame atualizado de volta ao CSV
+    df.to_csv(caminho_perguntas)
+
+    print(quiz)
+
+def play_quiz(perguntas_select):
+
+    for i, o in enumerate(perguntas_select):
+        menu_pergunta = f'''
+{i+1}) {o["Pergunta"]}
+    A){o["Alternativa A"]}
+    B){o["Alternativa B"]}
+    C){o["Alternativa C"]}
+    D){o["Alternativa D"]}
+        '''
+        
+        print(menu_pergunta)
+        user_resp = input("Sua resposta --> ").strip().capitalize()
+        if user_resp != o["Correta"]:
+            print("RESPOSTA INCORRETA...")
+        else:
+            print("RESPOSTA CORRETA...")
+    print("Fim do quiz. Volte amanhã :)")
+    global fez_quiz_diario
+    fez_quiz_diario = True
 
 
+caminho_notebook = os.path.join("dados", "notebook.csv") # Criando um caminho_notebook valido tanto para windows quanto para linux
+caminho_perguntas = os.path.join("dados", "perguntas.csv")
 
-logo = '''
-██╗  ██╗ █████╗ ██╗    ██╗ █████╗ ██╗██╗
-██║  ██║██╔══██╗██║    ██║██╔══██╗██║██║
-███████║███████║██║ █╗ ██║███████║██║██║
-██╔══██║██╔══██║██║███╗██║██╔══██║██║██║
-██║  ██║██║  ██║╚███╔███╔╝██║  ██║██║██║
-╚═╝  ╚═╝╚═╝  ╚═╝ ╚══╝╚══╝ ╚═╝  ╚═╝╚═╝╚═╝
-'''
 
-menu = '''
-Escolha uma alternativa:
-1- Pesquisar uma palavra no dicionario;
-2- Acessar Notebook;
-3- Limpar Notebook;
-4- Realizar quiz diario;
-5- Sair.
-'''
+hoje = date.today().strftime("%b-%d-%Y")
 
-print(logo)
+
+print(menu.logo)
+
+fez_quiz_diario = False
 
 while True:
-    user_act = int(input(menu))
+    user_act = int(input(menu.opcoes))
     match user_act:
         case 1:
             palavra = input("Digite a palavra: ")
-            req_mostrar_palavra(palavra)
+            req_mostrar_palavra(palavra, caminho_notebook)
         case 2:
-            acessar_notebook()
+            acessar_notebook(caminho_notebook)
         case 3:
-            delete_notebook()
+            delete_notebook(caminho_notebook)
         case 4:
-            hoje = date.today().strftime("%b-%d-%Y")
-            acessar_quiz_diario(hoje)
+            if fez_quiz_diario:
+                print("Você ja fez o quiz diario. Volte amanhã :)")
+            else:
+                gerar_quiz_diario(caminho_perguntas)
+                fez_quiz_diario = True
         case 5:
             print("Até a proxima :)")
             break
